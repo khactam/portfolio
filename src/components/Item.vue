34d3618fe2ce7aaf5ed089169f9f1d8cba44aa55
khactam <khactam95@gmail.com>
2019-04-01 17:36:57 +0300

    first add some amazing stuff

---
+++ src/components/Item.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <md-card class="card-container" md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">{{this.$props.bundle.title}}</div>
+          <div class="md-subhead">{{this.$props.bundle.subTitle}}</div>
+        </md-card-header>
+
+        <md-card-content>
+          {{this.$props.bundle.description}}
+        </md-card-content>
+      </md-ripple>
+    </md-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Item',
+  props: {
+    bundle: Object
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+.card-container{
+  width: 200px;
+  height: 250px;
+  margin: 0 0 0 0;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="cards-container">
+    <div class="card-container" v-for="(bundle, index) in this.bundles" :key="index">
+      <item :bundle="bundle"></item>
+    </div>
+
+  </div>
+</template>
+<script>
+import Item from './Item.vue'
+export default {
+  name: 'Main',
+  components: {Item},
+  data () {
+    return {
+      bundles: [
+        {
+          title: 'Education',
+          subTitle: 'Wonder schools.',
+          description: 'Education is the most important part of my life. Know how to Math and love Physics. Used to good at Chemistry and never understand Biology.'
+        },
+        {
+          title: 'Career',
+          subTitle: 'Panic path.',
+          description: 'I did not choose the programming life, the programming life chose me. Also being an entrepreneur so life is as coffee and milk.'
+        },
+        {
+          title: 'GoodAt',
+          subTitle: 'Things I know how.',
+          description: 'Skill set that I know, languages that I speak, also include a few things I randomly put to attract headhunters.'
+        },
+        {
+          title: 'LoveTo',
+          subTitle: 'Stuff I love',
+          description: 'Things I like to do everyday, just like another guy saying what he loves to do to attract headhunters, but I do love them.'
+        }
+      ]
+    }
+  },
+  mounted () {
+    console.log(this.bundles)
+  }
+}
+</script>
+<style scoped>
+.cards-container{
+  width: 450px;
+  text-align: center;
+  margin: auto;
+}
+.card-container{
+  display: inline-block;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+</style>
