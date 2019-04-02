fe5d8c341c52ec227a9c9da088f24d5a0db39fa4
khactam <khactam95@gmail.com>
2019-04-02 10:33:26 +0300

    Put Main view with all view components

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,44 +1,63 @@
 <template>
   <div class="cards-container">
     <div class="card-container" v-for="(bundle, index) in this.bundles" :key="index">
-      <item :bundle="bundle"></item>
+      <item :bundle="bundle" @click.native="itemOnClick(bundle)"></item>
     </div>
-
+    <education :toShow="this.bundles[0].toShow" @viewClicked="onViewClicked"></education>
+    <career :toShow="this.bundles[1].toShow" @viewClicked="onViewClicked"></career>
+    <good-at :toShow="this.bundles[2].toShow" @viewClicked="onViewClicked"></good-at>
+    <love-to :toShow="this.bundles[3].toShow" @viewClicked="onViewClicked"></love-to>
   </div>
 </template>
 <script>
 import Item from './Item.vue'
+import Education from './Education.vue'
+import Career from './Career.vue'
+import GoodAt from './GoodAt.vue'
+import LoveTo from './LoveTo.vue'
 export default {
   name: 'Main',
-  components: {Item},
+  components: {Education, Item, Career, GoodAt, LoveTo},
   data () {
     return {
       bundles: [
         {
           title: 'Education',
           subTitle: 'Wonder schools.',
-          description: 'Education is the most important part of my life. Know how to Math and love Physics. Used to good at Chemistry and never understand Biology.'
+          description: 'Education is the most important part of my life. Know how to Math and love Physics. Used to good at Chemistry and never understand Biology.',
+          toShow: false
         },
         {
           title: 'Career',
           subTitle: 'Panic path.',
-          description: 'I did not choose the programming life, the programming life chose me. Also being an entrepreneur so life is as coffee and milk.'
+          description: 'I did not choose the programming life, the programming life chose me. Also being an entrepreneur so life is as coffee and milk.',
+          toShow: false
         },
         {
           title: 'GoodAt',
           subTitle: 'Things I know how.',
-          description: 'Skill set that I know, languages that I speak, also include a few things I randomly put to attract headhunters.'
+          description: 'Skill set that I know, languages that I speak, also include a few things I randomly put to attract headhunters.',
+          toShow: false
         },
         {
           title: 'LoveTo',
           subTitle: 'Stuff I love',
-          description: 'Things I like to do everyday, just like another guy saying what he loves to do to attract headhunters, but I do love them.'
+          description: 'Things I like to do everyday, just like another guy saying what he loves to do to attract headhunters, but I do love them.',
+          toShow: false
         }
       ]
     }
   },
   mounted () {
-    console.log(this.bundles)
+  },
+  methods: {
+    itemOnClick: function (item) {
+      item.toShow = !(item.toShow)
+    },
+    onViewClicked: function (data) {
+      let bundle = this.bundles.find(bundle => bundle.title === data.title)
+      bundle.toShow = data.toShow
+    }
   }
 }
 </script>
