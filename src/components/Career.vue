877a210e3879418aba351cc30816d98b76d910ca
khactam <khactam95@gmail.com>
2019-04-02 10:32:03 +0300

    Add view components

---
+++ src/components/Career.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <md-dialog :md-active.sync="this.toShowData">
+      <md-dialog-title>Career</md-dialog-title>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="onClosed">Close</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  </div>
+</template>
+<script>
+export default {
+  Name: 'Career',
+  props: {
+    toShow: Boolean
+  },
+  watch: {
+    toShow: function (newVal, oldVal) {
+      let vm = this
+      if (newVal) vm.toShowData = newVal
+    }
+  },
+  data () {
+    return {
+      toShowData: false
+    }
+  },
+  methods: {
+    onClosed: function () {
+      this.toShowData = false
+      this.$emit('viewClicked', {toShow: false, title: 'Career'})
+    }
+  },
+  mounted () {
+    var vm = this
+    if (vm.$props.toShow) vm.toShowData = vm.$props.toShow
+  }
+}
+</script>
---
+++ src/components/GoodAt.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <md-dialog :md-active.sync="this.toShowData">
+      <md-dialog-title>GoodAt</md-dialog-title>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="onClosed">Close</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  </div>
+</template>
+<script>
+export default {
+  Name: 'GoodAt',
+  props: {
+    toShow: Boolean
+  },
+  watch: {
+    toShow: function (newVal, oldVal) {
+      let vm = this
+      if (newVal) vm.toShowData = newVal
+    }
+  },
+  data () {
+    return {
+      toShowData: false
+    }
+  },
+  methods: {
+    onClosed: function () {
+      this.toShowData = false
+      this.$emit('viewClicked', {toShow: false, title: 'GoodAt'})
+    }
+  },
+  mounted () {
+    var vm = this
+    if (vm.$props.toShow) vm.toShowData = vm.$props.toShow
+  }
+}
+</script>
